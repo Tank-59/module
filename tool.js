a82db71d123a3e0b6e605085f490b4ffffc7f4e4
59下山 <caoyanqi15@126.com>
2019-12-25 18:08:35 +0800

    Create tool.js

---
+++ tool.js
@@ -0,0 +1,9 @@
+// 获取地址参数
+export function getParameters(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    }
+    return "";
+}
