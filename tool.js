8f5c14573ef8d4a9138ca0ed80ecb4571b93dfcb
59下山 <caoyanqi15@126.com>
2020-01-09 18:13:37 +0800

    add deep copy

--- tool.js
+++ tool.js
@@ -19,3 +19,40 @@ export function arrayInterchange(arr, index1, index2) {
     arr[index1] = arr.splice(index2, 1, arr[index1])[0];
     return arr;
 }
+
+/**
+ * 深拷贝
+ * Deep copy the given object considering circular structure.
+ * This function caches all nested objects and its copies.
+ * If it detects circular structure, use cached copy to avoid infinite loop.
+ *
+ * @param {*} obj
+ * @param {Array<Object>} cache
+ * @return {*}
+ */
+ function deepCopy (obj, cache = []) {
+    // just return if obj is immutable value
+    if (obj === null || typeof obj !== 'object') {
+      return obj
+    }
+  
+    // if obj is hit, it is in circular structure
+    const hit = cache.find(c => c.original === obj)
+    if (hit) {
+      return hit.copy
+    }
+  
+    const copy = Array.isArray(obj) ? [] : {}
+    // put the copy into cache at first
+    // because we want to refer it in recursive deepCopy
+    cache.push({
+      original: obj,
+      copy
+    })
+  
+    Object.keys(obj).forEach(key => {
+      copy[key] = deepCopy(obj[key], cache)
+    })
+  
+    return copy
+  }
